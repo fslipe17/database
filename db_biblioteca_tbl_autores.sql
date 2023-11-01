8c2e0e17dfc2518daabe8d6d6fae810eb8329870
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2023-11-01 10:58:50 -0300

    Add files via upload

---
+++ db_biblioteca_tbl_autores.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: db_biblioteca
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_autores`
+--
+
+DROP TABLE IF EXISTS `tbl_autores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_autores` (
+  `ID_Autor` smallint NOT NULL,
+  `Nome_autor` varchar(50) NOT NULL,
+  `Sobrenome_Autor` varchar(60) NOT NULL,
+  PRIMARY KEY (`ID_Autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_autores`
+--
+
+LOCK TABLES `tbl_autores` WRITE;
+/*!40000 ALTER TABLE `tbl_autores` DISABLE KEYS */;
+INSERT INTO `tbl_autores` VALUES (1,'Roberta','Fonseca'),(2,'Caroline','Tudor'),(3,'Neil','Gaiman'),(4,'Matthew','Perry'),(5,'Jenna','Evans Welch'),(6,'Stephen','King'),(7,'Herman','Melville'),(8,'George','Orwell'),(9,'Mary','Shelley'),(10,'Cecília','Turner'),(11,'Aluísio','Azevedo'),(12,'Matt','Haig'),(13,'George','Saunders');
+/*!40000 ALTER TABLE `tbl_autores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-01 10:57:42
---
+++ db_biblioteca_tbl_livro.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: db_biblioteca
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_livro`
+--
+
+DROP TABLE IF EXISTS `tbl_livro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_livro` (
+  `ID_livro` smallint NOT NULL AUTO_INCREMENT,
+  `Nome_Livro` varchar(50) NOT NULL,
+  `ISBN` varchar(30) NOT NULL,
+  `ID_autor` smallint NOT NULL,
+  `Data_pub` date NOT NULL,
+  `Preco_livro` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`ID_livro`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_livro`
+--
+
+LOCK TABLES `tbl_livro` WRITE;
+/*!40000 ALTER TABLE `tbl_livro` DISABLE KEYS */;
+INSERT INTO `tbl_livro` VALUES (1,'Homem de giz','19',3,'2018-01-26',40),(2,'Homem de giz','19',3,'2018-01-26',39),(3,'Coraline','18',4,'2002-07-02',49),(4,'Friends, Lovers and the Big Terrible Thing','17',5,'2022-11-01',82),(5,'Amor & gelato','16',6,'2016-05-03',49),(6,'It: A Coisa','15',7,'1986-09-15',99),(7,'Moby Dick','14',8,'1851-10-18',25),(8,'1984','13',9,'1949-06-08',25),(9,'Frankenstein','12',10,'2008-08-27',50),(10,'Zé colméia e Catatau: Aventuras Na Floresta','11',9,'1994-01-01',15),(11,'O Cortiço','10',8,'1990-01-01',45),(12,'A Biblioteca da Meia-Noite','09',8,'2020-08-13',42),(13,'Dez de Dezembro','08',7,'2013-12-10',35);
+/*!40000 ALTER TABLE `tbl_livro` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-01 10:57:42
